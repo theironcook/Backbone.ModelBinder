772fa81380cb42c80fc7b0945a994374cc204324
Simon Boudrias <admin@simonboudrias.com>
2012-04-17 20:56:10 -0400

    Easier to read this way around

--- Backbone.ModelBinder.js
+++ Backbone.ModelBinder.js
@@ -113,11 +113,11 @@
 
                 for (bindingCount = 0; bindingCount < attributeBinding.elementBindings.length; bindingCount++) {
                     elementBinding = attributeBinding.elementBindings[bindingCount];
-                    if (elementBinding.selector !== '') {
-                        foundEls = $(elementBinding.selector, this._rootEl);
+                    if (elementBinding.selector === '') {
+						foundEls = $(this._rootEl);
                     }
                     else {
-                        foundEls = $(this._rootEl);
+                        foundEls = $(elementBinding.selector, this._rootEl);
                     }
 
                     if (foundEls.length === 0) {

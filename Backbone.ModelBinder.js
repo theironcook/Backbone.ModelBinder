247b2d85b84ab94259a2b1ef5e7986dc91bfeaa2
Kubeł Śmieci <kubelsmieci@gmail.com>
2012-08-07 02:18:47 +0200

    removing 'checked' attr from input[type="radio"] when setting value is different from el.val()
    Now behaviour will be similar to input[type="checkbox"].
    This will allow correct behaviour with radios when calling model.unset
    or model.set with value not existing in radios - after this operations
    none radio will be checked.

--- Backbone.ModelBinder.js
+++ Backbone.ModelBinder.js
@@ -310,6 +310,9 @@
                         if (el.val() === convertedValue) {
                             el.attr('checked', 'checked');
                         }
+                        else {
+                            el.removeAttr('checked');
+                        }
                         break;
                     case 'checkbox':
                         if (convertedValue) {
